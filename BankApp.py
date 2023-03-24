e4d3a4e7ae6a380278489547973ba82a39aff277
barmal <barmal.1992@wp.pl>
2023-03-24 18:17:39 +0100

    Fixed UI as a class, updated regex for holder's name. Created a way for the break in the while function

--- BankApp.py
+++ BankApp.py
@@ -1,6 +1,9 @@
 import re
+import os
+
+
 class BankAccount:
-    def __init__(self, acc_num, balance, owner, pswd):
+    def __init__(self, acc_num, balance, owner: str, pswd):
         self._acc_num = acc_num
         self._balance = balance
         self._owner = owner
@@ -44,7 +47,7 @@ class Bank:
 
     def add_new_acc(self):
         reg_numbers = r'^\d+$'
-        reg_names = r'^[a-zA-Z]+[ ]?[a-zA-Z]*$'
+        reg_names = r'^[ ]*[a-zA-Z]+[ ]+[a-zA-Z]*[ ]*$'
 
         print("Enter the new account number: ", end='')
         num = self._input_w_regex(self, reg_numbers, "Only digits in the account number! Try again.")
@@ -54,6 +57,8 @@ class Bank:
 
         print("Enter the holder name (first name, last name): ", end='')
         nam = self._input_w_regex(self, reg_names, "First name, last name, only letters! Try again.")
+        nam = nam.lower().lstrip().rstrip()
+        nam = " ".join(list(map(lambda x: x.capitalize(), nam.split())))
 
         print("Enter the password: ", end='')
         _pswd = str(input())
@@ -92,37 +97,61 @@ class Bank:
 
 class BankUI:
     all_requests = (
-        (1, 'create', '1'),
-        (2, 'withdraw', '2'),
-        (3, 'deposit', '3'),
-        (4, 'delete', '4'),
-        (5, 'exit', '5'),
-        (6, 'showdb', '6')
+        ('1', 'create'),
+        ('2', 'withdraw'),
+        ('3', 'deposit'),
+        ('4', 'delete'),
+        ('5', 'exit'),
+        ('6', 'showdb')
     )
-
-    def __init__(self):
-        pass
 
     def print_menu(self, bank):
         print("Hello, welcome in our Bank {bank_name}".format(bank_name=bank))
-        print("What would you like to do?: \n"
-              "1. <create> an account \n"
-              "2. <withdraw> \n"
-              "3. <deposit> \n"
-              "4. <delete> \n"
-              "5. <exit> \n"
-              "6. <showdb>", end='\n')
+        print("What would you like to do?: ")
+        for pos in self.all_requests:
+            print(f'{pos[0]}. <{pos[1]}>')
 
     def request_chooser(self, request):
         switch_case = str(request).lower().lstrip().rstrip()
         for req in self.all_requests:
-            if switch_case == req[2]:
-                return switch_case
+            if switch_case in req:
+                return req[1]
         else:
             print("No such request.")
 
-    def req_create(self):
-        pass
+    def request_handling(self, request, the_bank):
+        switch_case = self.request_chooser(request)
+        if switch_case == 'create':
+            os.system('cls')
+            the_bank.add_new_acc()
+            print('-'*20+'\n'+"Account successfully created")
+            print(the_bank.get_last_acc())
+            print('-'*20)
+            return True
+        elif switch_case == 'withdraw':
+            print("Log in to you account by 1. <accnum> 2. <name>")
+            searchby = str(input()).lower().lstrip().rstrip()
+            print("Write the account number: ")
+            accnum = str(input()).lower().lstrip().rstrip()
+            if searchby == '1' or searchby == 'accnum':
+                print(the_bank.search_by_num(accnum))
+            return True
+        elif switch_case == 'deposit':
+            return True
+        elif switch_case == 'delete':
+            return True
+        elif switch_case == 'exit':
+            print("Bye then")
+            return False
+        elif switch_case == 'showdb':
+            print("Password: ")
+            if input() == '123123':
+                print('\n'+"-"*20)
+                the_bank.print_the_database()
+                print("-"*20, end='\n\n')
+            else:
+                print("Access denied")
+            return True
 
     def req_withdraw(self):
         pass
--- BankApp_test.py
+++ BankApp_test.py
@@ -1,43 +1,15 @@
 from BankApp import BankAccount, Bank, BankUI
-import os
 
 mybank = Bank("WallStreetBank")
 myui = BankUI()
-# print(mybank.acc_db[0].get_owner())
 print(myui.request_chooser('2'))
 
 while True:
     myui.print_menu(mybank.get_name())
-    request = myui.request_chooser(str(input()))
-
-    if request == 'create' or request == '1':
-        os.system('cls')
-        mybank.add_new_acc()
-        print("Account successfully created")
-        print(mybank.get_last_acc())
-
-    elif request == 'withdraw' or request == '2':
-        print("Log in to you account by 1. <accnum> 2. <name>")
-        searchby = str(input()).lower().lstrip().rstrip()
-        print("Write the account number: ")
-        accnum = str(input()).lower().lstrip().rstrip()
-        if searchby == '1' or searchby == 'accnum':
-            print(mybank.search_by_num(accnum))
-
-    elif request == 'exit' or request == '5':
+    stay = myui.request_handling(str(input()), mybank)
+    if not stay:
         break
 
-    elif request == 'showdb' or request == '6':
-        print("Password: ")
-        if input() == '123123':
-            print('\n'+"-"*20)
-            mybank.print_the_database()
-            print("-"*20, end='\n\n')
-        else:
-            print("Access denied")
-    else:
-        os.system('cls')
-        print("<<< Sorry there's no such request. Try again >>>")
 
 
 
